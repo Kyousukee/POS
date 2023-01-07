76343c1a33f6aef7da96fde8cf4cfd7f4e9338f9
Kyousukee <jjosemiguel.jv@gmail.com>
2023-01-06 22:59:45 -0300

    setting

--- src/app/components/reusable/global-constants.ts
+++ src/app/components/reusable/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
-    // public static apiURL: string = "https://www.posapi.somee.com";
-    public static apiURL: string = "https://localhost:44379";
+    public static apiURL: string = "https://www.posapi.somee.com";
+    // public static apiURL: string = "https://localhost:44379";
       
     // public static siteTitle: string = "This is example of ItSolutionStuff.com";
 }
